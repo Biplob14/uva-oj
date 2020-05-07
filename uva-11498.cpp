cdf8c5f5dd366824a926abd28756865f2e2721ac
Sadekur Rahaman <biploburrahaman@gmail.com>
2020-05-07 22:30:49 +0600

    Create uva-11498.cpp

---
+++ uva-11498.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int k, n, m, x, y, i;
+
+    while(cin>>k){
+        if(k == 0) break;
+        cin>>n>>m;
+        for(i=0;i<k;i++){
+            cin>>x>>y;
+            if(x == n || y == m) cout<<"divisa"<<endl;
+            else if(x<n && y>m) cout<<"NO"<<endl;
+            else if(x>n && y>m) cout<<"NE"<<endl;
+            else if(x<n && y<m) cout<<"SO"<<endl;
+            else if(x>n && y<m) cout<<"SE"<<endl;
+        }
+    }
+
+    return 0;
+}
